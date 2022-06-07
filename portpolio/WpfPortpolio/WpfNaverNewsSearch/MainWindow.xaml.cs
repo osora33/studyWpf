0ac0ce572f3c1dabf999cdc73ccc19d7664829b3
소라 오 <osora33@gmail.com>
2022-06-07 10:44:43 +0900

    네이버 뉴스검색 앱 마무리

--- portpolio/WpfPortpolio/WpfNaverNewsSearch/MainWindow.xaml.cs
+++ portpolio/WpfPortpolio/WpfNaverNewsSearch/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using MahApps.Metro.Controls;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -42,7 +44,7 @@ namespace WpfNaverNewsSearch
             string keyword = txtSearch.Text;
             string clientID = "psCgSgFOexa7LSPNMbud";
             string clientSecret = "W2ldmPdnw9";
-            string openApiUri = $"https://openapi.naver.com/v1/search/news.json?start=1&display=100&query={keyword}";
+            string openApiUri = $"https://openapi.naver.com/v1/search/news.json?start={txtStartNum.Text}&display=10&query={keyword}";
             string result;
 
             WebRequest request = null;
@@ -73,9 +75,52 @@ namespace WpfNaverNewsSearch
                 response.Close();
             }
 
-            MessageBox.Show(result);
+            //MessageBox.Show(result);
 
-            //JObject parsedJson = JObject.Parse(result); //string to json
+            var parsedJson = JObject.Parse(result); //string to json
+            int total = Convert.ToInt32(parsedJson["total"]);       //전체 검색결과수
+            int display = Convert.ToInt32(parsedJson["display"]);
+
+            var items = parsedJson["items"];
+            var json_array = (JArray)items;
+
+            List<NewsItem> newsItems = new List<NewsItem>();        //데이터그리드 연동
+
+            foreach (var item in json_array)
+            {
+                var temp = DateTime.Parse(item["pubDate"].ToString());
+                NewsItem news = new NewsItem()
+                {
+                    Title = item["title"].ToString(),
+                    OriginalLink = item["originallink"].ToString(),
+                    Link = item["link"].ToString(),
+                    Description = item["description"].ToString(),
+                    PubDate = temp.ToString("yyyy-MM-dd HH:mm")
+                };
+
+                newsItems.Add(news);
+            }
+
+            this.DataContext = newsItems;
         }
+
+        private void dgrResult_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            if(dgrResult.SelectedItem == null)
+            {
+                return;
+            }
+            string link = (dgrResult.SelectedItem as NewsItem).Link;
+            Process.Start(link);
+        }
+    }
+
+    internal class NewsItem
+    {
+        public string Title { get; set; }
+        public string OriginalLink { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+        public string PubDate { get; set; }
     }
 }
